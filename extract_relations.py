3693929b8152ec012ab819182d2f8930b9d1d485
GeCui-1 <gcui6@outlook.com>
2025-03-09 17:19:47 -0400

    Update extract_relations.py

--- extract_relations.py
+++ extract_relations.py
@@ -1,7 +1,7 @@
 import os
 
 entities_file_path = "./data/treatment/subjects.data"
-context_file_path = "./data/context.csv"
+context_file_path = "./data/context_of_disease_which_has_relation_to_genes.csv"
 extracted_entity_log_path = "./data/treatment/extracted_entity.log"
 extracted_context_log_path = "./data/treatment/extracted_context.log"
 
