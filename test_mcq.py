975d3e7dc6ecff59116bea3eaf681bdf85f5f821
GeCui-1 <gcui6@outlook.com>
2025-03-12 11:37:50 -0400

    Update test_mcq.py

--- test_mcq.py
+++ test_mcq.py
@@ -36,7 +36,7 @@ for i in range(len(contexts)):
         rag.insert(buffer)
         time.sleep(10)
         buffer = ""
-        break;
+        # break;
 if len(buffer) > 0:
     # print("buffer:", buffer)
     rag.insert(buffer)
