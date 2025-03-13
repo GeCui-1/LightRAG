e8b373c510b7961d28c635364a1160f5ba2f0b6a
GeCui-1 <gcui6@outlook.com>
2025-03-13 00:45:30 -0400

    Create test_mcq2.py

---
+++ test_mcq2.py
@@ -0,0 +1,103 @@
+import os
+from lightrag import LightRAG, QueryParam
+from lightrag.llm import gpt_4o_mini_complete
+from lightrag.data_loader import get_true_false_data, get_mcq_data
+import time
+#########
+# Uncomment the below two lines if running in a jupyter notebook to handle the async nature of rag.insert()
+# import nest_asyncio
+# nest_asyncio.apply()
+#########
+
+# meta data for the test
+num_questions = 1000  # totally 311 questions
+query_mode = 'naive' # choose from naive, local, global, hybrid
+
+WORKING_DIR = "./tf"
+if not os.path.exists(WORKING_DIR):
+    os.mkdir(WORKING_DIR)
+
+context_file_path = "./data/mcq_entity.log"
+
+#################################################################### create RAG
+rag = LightRAG(
+    working_dir=WORKING_DIR,
+    llm_model_func=gpt_4o_mini_complete,  # Use gpt_4o_mini_complete LLM model
+    # llm_model_func=gpt_4o_complete  # Optionally, use a stronger model
+)
+with open(context_file_path, "r", encoding="utf-8") as f:
+    raw_contexts = f.read()
+contexts = raw_contexts.split("\n")
+buffer = ""
+print("context length:", len(contexts))
+for i in range(min(225000, len(contexts))):
+    buffer += contexts[i] + "\n"
+    if i % 5000 == 4999:
+        rag.insert(buffer)
+        time.sleep(10)
+        buffer = ""
+        # break;
+if len(buffer) > 0:
+    # print("buffer:", buffer)
+    rag.insert(buffer)
+#################################################################### create RAG
+
+# Load questions and answers
+dataset_paths = {
+    "cancer": "./data/mcq/mcq_cancer.data",
+    "gene": "./data/mcq/mcq_genetic_disorder.data",
+    "heart": "./data/mcq/mcq_heart.data",
+    "immune": "./data/mcq/mcq_immune.data",
+    "infection": "./data/mcq/mcq_infection.data",
+    "neural": "./data/mcq/mcq_neural.data",
+}
+
+qas = {}
+corrects = {}
+wrongs = {}
+unsures = {}
+exceptions = {}
+total_times = {}
+for key, val in dataset_paths.items():
+    questions, answers = get_mcq_data(dataset_paths[key])
+    qas[key] = (questions, answers)
+    corrects[key] = 0
+    wrongs[key] = 0
+    unsures[key] = 0
+    exceptions[key] = 0
+    total_times[key] = 0.0
+
+for qa_type, qa in qas.items():
+    print("qa type:", qa_type)
+    questions = qa[0]
+    labels = qa[1]
+    print("questions:", questions)
+    print("answers:", answers)
+    for i in range(min(num_questions, len(questions))):
+        print("")
+        print("instance number ", i)
+        
+        question = questions[i] + ". Your response should start with the answer. Do not mention genes or variants that are not the answer."
+        label = labels[i].lower()
+        start_time = time.time()
+        
+        ans = rag.query(question, param=QueryParam(mode=query_mode))
+        ans = ans.split('.')[0]
+        
+        end_time = time.time()
+        total_times[qa_type] += end_time - start_time
+    
+        print("question: ", question)
+        print("label: ", label)
+        print("ans: ", ans)
+      
+        if label in ans.lower():
+            print("answer is correct")
+            corrects[qa_type] += 1
+        else:
+            print("answer is wrong")
+            wrongs[qa_type] += 1
+
+print("correct count: ", corrects)
+print("wrong count: ", wrongs)
+print("total time: ", total_times)
